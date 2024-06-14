8d61928d66b60cd44704b9c57f2d1608c4d49c2e
Simal-Arif <you@example.com>
2024-06-14 10:07:18 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+// assignment 2:
+// Personal message:Store a person's name in a variable, and print a message to that person.Your message should
+// be simple,such as,"Hello Eric, would you like to learn some python today?"
+var personName = "Eric";
+// printing a message
+console.log("Hello ".concat(personName, ",would you like to learn some python today?"));
---
+++ main.ts
@@ -0,0 +1,8 @@
+// assignment 2:
+
+// Personal message:Store a person's name in a variable, and print a message to that person.Your message should
+// be simple,such as,"Hello Eric, would you like to learn some python today?"
+
+let personName:string = "Eric";
+// printing a message
+console.log(`Hello ${personName},would you like to learn some python today?`);
