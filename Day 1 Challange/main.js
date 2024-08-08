bbed530d1e2671dfb2c18891f8e79c662e595756
Simal Arif <simalarif7@gmail.com>
2024-08-08 05:13:01 -0700

    Add files via upload

---
+++ Day 1 Challange/main.js
@@ -0,0 +1,17 @@
+// Question 1: Install node.js, typescript, vs code
+// Question 2: Personal message:
+/* Store a person's name in a variable and print a message to them, like "Hello Asharib, would you like to learn
+some typescript today?"*/
+var personName = "Mihrimah";
+// printing a messsage 
+console.log("\"Hello ".concat(personName, ", would you like to learn some typescript today?\""));
+// Question 3: Names Cases:
+/* Store a person's name in a variable, and then print that person's name in lowercase, uppercase, titlecase*/
+// storing a person name in a variable
+var name1 = "Simal arif";
+// in lower case
+console.log(name1.toLowerCase());
+// in upper case
+console.log(name1.toUpperCase());
+// in title case
+console.log(name1.charAt(0).toUpperCase() + name1.slice(1).toLowerCase());
---
+++ Day 1 Challange/main.ts
@@ -0,0 +1,22 @@
+// Question 1: Install node.js, typescript, vs code
+// Question 2: Personal message:
+/* Store a person's name in a variable and print a message to them, like "Hello Asharib, would you like to learn 
+some typescript today?"*/
+
+let personName: string ="Mihrimah";
+// printing a messsage 
+console.log(`"Hello ${personName}, would you like to learn some typescript today?"`);
+
+
+// Question 3: Names Cases:
+/* Store a person's name in a variable, and then print that person's name in lowercase, uppercase, titlecase*/
+
+// storing a person name in a variable
+let name1: string ="Simal arif";
+
+// in lower case
+console.log(name1.toLowerCase());
+// in upper case
+console.log(name1.toUpperCase());
+// in title case
+console.log(name1.charAt (0).toUpperCase() + name1.slice(1).toLowerCase());                                                                        
